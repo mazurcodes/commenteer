659a4b1c5d2c188f25d8e7ef8739c540553d03da
Bartlomiej <trafficap@gmail.com>
2023-06-16 12:37:08 +0200

    dynamic job page with jobId

--- app/j/[jobId]/page.tsx
+++ app/j/[jobId]/page.tsx
@@ -1,17 +1,53 @@
 'use client';
+import { auth } from '@/firebase/clientApp';
 import { useJob } from '@/firebase/crudHooks';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import styles from './page.module.scss';
+import Nav from '@/components/Nav';
+import TopBar from '@/components/TopBar';
+import JobDisplay from '@/components/JobDisplay';
+import WelcomeScreen from '@/components/WelcomeScreen';
 
-type JobPageProps = {
-  params: {
-    jobId: string;
-  };
-};
+const JobPage = () => {
+  const [user, loading, error] = useAuthState(auth);
 
-const JobPage = ({ params }: JobPageProps) => {
-  const [job, loading, error] = useJob(params.jobId);
-  if (loading) return <p>Loading...</p>;
-  //   if (error) return <p>Error: {error.message}</p>;
-  return <div>hello {job?.name}</div>;
+  if (loading) {
+    return (
+      <main className={styles.center}>
+        <p>Checking user...</p>
+      </main>
+    );
+  }
+
+  if (error) {
+    return (
+      <main className={styles.center}>
+        <p>
+          There was some error:{' '}
+          <span className="text-red-600">{error.message}</span>
+        </p>
+      </main>
+    );
+  }
+
+  if (user)
+    return (
+      <div className={styles.wrapper}>
+        <Nav />
+        <div className={styles.mainWrapper}>
+          <TopBar />
+          <main className={styles.main}>
+            <JobDisplay />
+          </main>
+        </div>
+      </div>
+    );
+
+  return (
+    <main className={styles.center}>
+      <WelcomeScreen />
+    </main>
+  );
 };
 
 export default JobPage;
