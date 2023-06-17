636ae95529ad68bb262c8733c331294966b04deb
Bartlomiej <trafficap@gmail.com>
2023-06-17 16:18:13 +0200

    NewJobButton as a link to the main page

--- app/components/Nav/JobsList/index.tsx
+++ app/components/Nav/JobsList/index.tsx
@@ -3,6 +3,9 @@ import { useUserJobs } from '@/firebase/crudHooks';
 import Job from './Job';
 import styles from './index.module.scss';
 import { auth } from '@/firebase/clientApp';
+import Link from 'next/link';
+import Image from 'next/image';
+import NewJobIcon from '@/assets/NewJobIcon.svg';
 
 const JobsList = () => {
   const [jobs, loading, error] = useUserJobs(auth.currentUser?.uid);
@@ -10,6 +13,10 @@ const JobsList = () => {
     <div className={styles.jobsWrapper}>
       <h2>Jobs</h2>
       <div className={styles.jobs}>
+        <Link href="/" className={styles.link}>
+          <Image src={NewJobIcon} height={20} width={20} alt="user icon" />
+          New Job
+        </Link>
         <p className={styles.jobsTimeframe}>Previous 30 days</p>
         {loading && 'Loading...'}
         {error && error.message}
