6599f925e6dac9a5f295e0d1fcb6e95fe168149c
Bartlomiej <trafficap@gmail.com>
2023-06-17 10:56:13 +0200

    page /j/ executes notFound functionality

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ const Page404 = () => {
   return (
     <div className={styles.wrapper}>
       <main className={styles.main}>
-        <h1 className={styles.logo}>Gifter</h1>
+        <h1 className={styles.logo}>Commenteer</h1>
         <h2 className={styles.title}>404</h2>
         <a href={'/'} className={styles.link}>
           Main Page
