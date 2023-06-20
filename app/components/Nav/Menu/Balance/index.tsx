582b5900fc4a1d97a2c10d2db0ff549cd3b32a53
Bartlomiej <trafficap@gmail.com>
2023-06-20 19:49:46 +0200

    Balance button as link

--- app/components/Nav/Menu/Balance/index.tsx
+++ app/components/Nav/Menu/Balance/index.tsx
@@ -1,28 +1,22 @@
-'use client';
-import { useState } from 'react';
 import styles from './index.module.scss';
 import Image from 'next/image';
 import BalanceIcon from '@/assets/BalanceIcon.svg';
+import Link from 'next/link';
 
-const NavMenuBalance = () => {
-  const [isOpen, setOpen] = useState(false);
-
+const MenuBalance = () => {
   return (
     <div className={styles.balance}>
-      <div className={isOpen ? styles.recharge : styles.hidden}>
-        this is recharge menu
-      </div>
-      <button className={styles.balanceBtn} onClick={() => setOpen(!isOpen)}>
-        <div className={styles.buttonLeft}>
+      <Link className={styles.balanceBtn} href="/balance">
+        <div className={styles.buttonSideLeft}>
           <Image src={BalanceIcon} height={20} width={20} alt="user icon" />
           <p>Balance</p>
         </div>
-        <div className={styles.buttonRight}>
+        <div className={styles.buttonSideRight}>
           <p>10$</p>
         </div>
-      </button>
+      </Link>
     </div>
   );
 };
 
-export default NavMenuBalance;
+export default MenuBalance;
--- app/components/Nav/Menu/index.tsx
+++ app/components/Nav/Menu/index.tsx
@@ -1,11 +1,11 @@
-import NavMenuBalance from './Balance';
+import MenuBalance from './Balance';
 import MenuUser from './User';
 import styles from './index.module.scss';
 
 const Menu = () => {
   return (
     <div role="menu" className={styles.menu}>
-      <NavMenuBalance />
+      <MenuBalance />
       <MenuUser />
     </div>
   );
