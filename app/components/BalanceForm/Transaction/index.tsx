f21b73f6a43918c5db197bb8ec0a32384e06733e
Bartlomiej <trafficap@gmail.com>
2023-06-27 17:12:43 +0200

    replace strings with enum values

--- app/components/BalanceForm/Transaction/index.tsx
+++ app/components/BalanceForm/Transaction/index.tsx
@@ -4,6 +4,7 @@ import { TransactionData } from '@/types';
 import MinusIcon from '@/assets/HistoryMinusIcon.svg';
 import PlusIcon from '@/assets/HistoryPlusIcon.svg';
 import { convertTimestampToDate } from '@/utils/dateUtils';
+import { TransactionType } from '@/data/constants';
 
 type TransactionProps = {
   data: TransactionData;
@@ -12,13 +13,13 @@ type TransactionProps = {
 const Transaction = ({ data }: TransactionProps) => {
   return (
     <div className={styles.transactionRow}>
-      {data.type === 'recharge' && (
+      {data.type === TransactionType.RECHAGE && (
         <Image src={PlusIcon} height={15} alt="Plus icon" />
       )}
-      {data.type === 'refund' && (
+      {data.type === TransactionType.REFUND && (
         <Image src={PlusIcon} height={15} alt="Plus icon" />
       )}
-      {data.type === 'purchase' && (
+      {data.type === TransactionType.PURCHASE && (
         <Image src={MinusIcon} height={15} alt="Minus icon" />
       )}
       <p className={styles.transactionAmount}>
