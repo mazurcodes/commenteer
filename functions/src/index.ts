bd7ff32dfcd6de684e15f5ad9870e43e1af2b342
Bartlomiej <trafficap@gmail.com>
2023-07-04 12:48:37 +0200

    firebase functions balanceCreated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,8 @@
-import * as functions from "firebase-functions";
+import { onDocumentCreated } from 'firebase-functions/v2/firestore';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const db = admin.firestore();
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +11,21 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const createBalance = onDocumentCreated(
+  'customers/{customerId}',
+  (event) => {
+    const customerId = event.params.customerId;
+    const userDoc = db.collection('balance').doc(customerId);
+    userDoc.set({
+      amount: 1,
+      currency: 'USD',
+    });
+    userDoc.collection('transaction-history').doc().set({
+      created: Date.now(),
+      type: 'recharge',
+      amount: 1,
+      name: 'Initial balance',
+    });
+  }
+);
