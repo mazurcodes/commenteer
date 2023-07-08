cc102edf865acca2a4347efe8b540c26994edd7c
Bartlomiej <trafficap@gmail.com>
2023-07-08 15:08:32 +0200

    BalanceForm buttons with ButtonSpinner after click

--- app/components/BalanceForm/index.tsx
+++ app/components/BalanceForm/index.tsx
@@ -5,33 +5,49 @@ import { useBalance } from '@/firebase/crudHooks';
 import { auth, db } from '@/firebase/clientApp';
 import TransactionHistory from './TransactionHistory';
 import { onSnapshot, addDoc, collection } from 'firebase/firestore';
+import { useState } from 'react';
+import ButtonSpinner from '@/assets/ButtonSpinner.svg';
 
 // test price: price_1NPpnqEIhD4GWlLxAauumyR4
 // live price: price_1NQqUGEIhD4GWlLxBlySFvCn
+// live preset 2$ price: price_1NRZxgEIhD4GWlLxlAL2i0hM
+// live preset 5$ price: price_1NRZzwEIhD4GWlLxlbsJ8UBG
+// live preset 10$ price: price_1NRa0WEIhD4GWlLxok3ZtObp
 
 const BalanceForm = () => {
   const [balance, loading, error] = useBalance(auth.currentUser?.uid);
+  const [loadingUI, setLoadingUI] = useState('');
 
-  const loadCheckout = async () => {
+  const loadCheckout = async (priceId: string) => {
     if (auth.currentUser) {
-      const docRef = await addDoc(
-        collection(db, 'customers', auth.currentUser.uid, 'checkout_sessions'),
-        {
-          mode: 'payment',
-          price: 'price_1NPpnqEIhD4GWlLxAauumyR4',
-          success_url: window.location.origin,
-          cancel_url: window.location.origin,
-        }
-      );
-      onSnapshot(docRef, (snap) => {
-        const { error, url } = snap.data() as { error: Error; url: string };
-        if (error) {
-          alert(`An error occured: ${error.message}`);
-        }
-        if (url) {
-          window.location.assign(url);
-        }
-      });
+      try {
+        setLoadingUI(priceId);
+        const docRef = await addDoc(
+          collection(
+            db,
+            'customers',
+            auth.currentUser.uid,
+            'checkout_sessions'
+          ),
+          {
+            mode: 'payment',
+            price: priceId,
+            success_url: window.location.origin,
+            cancel_url: window.location.origin,
+          }
+        );
+        onSnapshot(docRef, (snap) => {
+          const { error, url } = snap.data() as { error: Error; url: string };
+          if (error) {
+            alert(`An error occured: ${error.message}`);
+          }
+          if (url) {
+            window.location.assign(url);
+          }
+        });
+      } catch (error) {
+        console.log('Error connecting to Stripe: ', error);
+      }
     }
   };
 
@@ -51,6 +67,8 @@ const BalanceForm = () => {
     );
   }
 
+  // Balance buttons as separate component
+
   if (balance)
     return (
       <div className={styles.wrapper}>
@@ -60,18 +78,66 @@ const BalanceForm = () => {
             {balance?.amount} <span className={styles.currencySymbol}>$</span>
           </h2>
           <div className={styles.wrapperRow}>
-            <button className={styles.buttonAmount}>
-              <p>1 $</p>
+            <button
+              className={styles.buttonAmount}
+              onClick={() => loadCheckout('price_1NRZxgEIhD4GWlLxlAL2i0hM')}
+            >
+              {loadingUI === 'price_1NRZxgEIhD4GWlLxlAL2i0hM' ? (
+                <Image
+                  src={ButtonSpinner}
+                  alt="Loading spinner"
+                  height={30}
+                  width={30}
+                />
+              ) : (
+                <p>2 $</p>
+              )}
             </button>
-            <button className={styles.buttonAmount}>
-              <p>2 $</p>
+            <button
+              className={styles.buttonAmount}
+              onClick={() => loadCheckout('price_1NRZzwEIhD4GWlLxlbsJ8UBG')}
+            >
+              {loadingUI === 'price_1NRZzwEIhD4GWlLxlbsJ8UBG' ? (
+                <Image
+                  src={ButtonSpinner}
+                  alt="Loading spinner"
+                  height={23}
+                  width={23}
+                />
+              ) : (
+                <p>5 $</p>
+              )}
             </button>
-            <button className={styles.buttonAmount}>
-              <p>5 $</p>
+            <button
+              className={styles.buttonAmount}
+              onClick={() => loadCheckout('price_1NRa0WEIhD4GWlLxok3ZtObp')}
+            >
+              {loadingUI === 'price_1NRa0WEIhD4GWlLxok3ZtObp' ? (
+                <Image
+                  src={ButtonSpinner}
+                  alt="Loading spinner"
+                  height={25}
+                  width={25}
+                />
+              ) : (
+                <p>10 $</p>
+              )}
             </button>
           </div>
-          <button className={styles.addFundsBtn} onClick={() => loadCheckout()}>
-            Add funds
+          <button
+            className={styles.addFundsBtn}
+            onClick={() => loadCheckout('price_1NQqUGEIhD4GWlLxBlySFvCn')}
+          >
+            {loadingUI === 'price_1NQqUGEIhD4GWlLxBlySFvCn' ? (
+              <Image
+                src={ButtonSpinner}
+                alt="Loading spinner"
+                height={25}
+                width={25}
+              />
+            ) : (
+              <p>Add custom amount</p>
+            )}
           </button>
           <div className={styles.powered}>
             <p>powered by</p>
