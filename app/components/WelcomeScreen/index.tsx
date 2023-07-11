110ab52b5786f952beb9276612d76b1fe13b8c87
Bartlomiej <trafficap@gmail.com>
2023-07-11 13:04:38 +0200

    add WelcomeScreen new styles

--- app/components/WelcomeScreen/index.tsx
+++ app/components/WelcomeScreen/index.tsx
@@ -4,7 +4,11 @@ import styles from './index.module.scss';
 const WelcomeScreen = () => {
   return (
     <div className={styles.welcomeWrapper}>
-      <h1>Welcome</h1>
+      <h1>Commenteer</h1>
+      <div className={styles.description}>
+        <p>Create Social Media Engagement with</p>
+        <p>AI Generated Comments</p>
+      </div>
       <div className={styles.btnWrapper}>
         <Link href="/login" className={styles.welcomeBtn}>
           Login
