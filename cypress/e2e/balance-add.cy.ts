3525339f75bae7a3e55fe11d4003e354bc19d318
Bartlomiej <trafficap@gmail.com>
2023-07-21 14:59:35 +0200

    add cypress balance final

--- cypress/e2e/balance-add.cy.ts
+++ cypress/e2e/balance-add.cy.ts
@@ -1,22 +1,43 @@
+// Login in the app first
+
 describe('User add money to balance by Stripe', () => {
   it('shows the balance page', () => {
     cy.visit('http://localhost:3000/balance');
     cy.get('h1').should('contain', 'Current balance');
     cy.get('h2').should('contain', 'Commenteer');
-
     cy.get('a[href="/balance"]').should('contain', 'Balance');
   });
 
   it('shows preset amount button row', () => {
     cy.visit('http://localhost:3000/balance');
-    cy.get('input[name="login-email"]').type('test@mazur.codes');
-    cy.get('input[name="login-password"]').type('wrongpassword');
-    cy.get('input[type="submit"]').click();
-    cy.get('span').should('contain', 'Error:');
+    cy.get('p').should('contain', 'Add preset amount:');
+    cy.get('button').should('contain', '2 $');
+    cy.get('button').should('contain', '5 $');
+    cy.get('button').should('contain', '10 $');
   });
 
-  // shows the custom amoutn button
-  // shows the flow information
-  // shows the payment icons
-  // shows the transaction history
+  it('shows custom amount button that redirects to stripe.com', () => {
+    cy.visit('http://localhost:3000/balance');
+    cy.get('button').should('contain', 'Add custom amount');
+    cy.get('button:contains("Add custom amount")').click();
+    cy.get('h1').should('contain', 'Redirecting to Stripe');
+  });
+
+  it('shows the flow information component that can open', () => {
+    cy.visit('http://localhost:3000/balance');
+    cy.get('p').should('contain', 'Check, what will happen');
+    cy.get('p').contains('After clicking').should('not.exist');
+    cy.get('p:contains("Check, what will happen")').click();
+    cy.get('p:contains("After clicking")').should('exist');
+  });
+
+  it('shows the payment icons', () => {
+    cy.visit('http://localhost:3000/balance');
+    cy.get('[alt*="payment icon"]').should('have.length', 5);
+  });
+
+  it('shows the transaction history', () => {
+    cy.visit('http://localhost:3000/balance');
+    cy.get('p').contains('History').should('exist');
+  });
 });
